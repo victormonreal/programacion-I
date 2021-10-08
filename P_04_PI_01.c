05f1ace25db63a89476d782b7882b30aac81572e
victormonreal <32993403+victormonreal@users.noreply.github.com>
2021-10-08 01:21:11 -0500

    Update P_04_PI_01.c

--- P_04_PI_01.c
+++ P_04_PI_01.c
@@ -2,17 +2,24 @@
 #include <math.io>
 
 /*
-Objetivo: Obtener el valor de PI con el siguiente producto (preguntar cu·ntos tÈrminos se desean)
+Objetivo: Obtener el valor de PI con el siguiente producto (preguntar cu√°ntos t√©rminos se desean)
 PI = 2(2/1)(2/3)(4/3)(4/5)(6/5)(6/7)(8/7)(8/9)...
 */
 
-int main()
+void ClrScr();
 
-{
-	
-	
-	
-	
-		
+int main(){
+	ClrScr();
+	printf("Cuantos terminos requieres?
 	return 0;
+}
+
+void ClrScr(){
+// Borra pantalla en Windows	
+#ifdef _WIN32
+	system("cls");
+// Borra pantalla en Linux
+#elif __linux__
+	system("clear");
+#endif
 }
